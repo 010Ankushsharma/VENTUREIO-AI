297a3cccdbea1ed2055cc8cf732086d3fd271d3d
010Ankushsharma <ankushsharma90285@gmail.com>
2026-07-30 23:02:21 +0530

    added  init for models

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.user import User
+from app.models.deal import Deal
+from app.models.document import Document
+from app.models.analysis import Analysis
+from app.models.report import Report
